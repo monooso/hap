6354238aff296a59a4b44421133d612656368313
Stephen Lewis <gpg@stephenlewis.me>
2023-02-21 12:59:20 +0000

    Use guest layout for home page

--- lib/hap_web/controllers/page_controller.ex
+++ lib/hap_web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule HapWeb.PageController do
   use HapWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home, layout: {HapWeb.Layouts, :guest})
   end
 end
